06bbc6c9ccecced2ff143a2614d41cf495360cee
Samuel Thissen <samuel.thissen.learn@gmail.com>
2026-07-09 11:51:44 +0200

    Split meal plans dashboard into its own /meal_plans page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,5 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
 
   def home
-    @chats = current_user.chats.order(created_at: :desc) if user_signed_in?
   end
 end
