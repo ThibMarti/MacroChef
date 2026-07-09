1a4e588d8d29466e3f39a1c8f12d8af07ee1bdb4
Thibault Martin <thibaultmartin02@hotmail.com>
2026-07-09 17:21:47 +0200

    Implemented the turbo stream and chunks

--- app/models/message.rb
+++ app/models/message.rb
@@ -4,14 +4,24 @@ class Message < ApplicationRecord
   belongs_to :chat
 
   validates :role, presence: true
-  validates :content, presence: true
+  validates :content, presence: true, unless: -> { role == "assistant" }
   validate :user_message_limit, if: -> { role == "user" }
+
+  after_create_commit :broadcast_append_to_chat
+
+  def broadcast_replace_to_chat
+    broadcast_replace_to chat, target: self, partial: "messages/message", locals: { message: self }
+  end
 
   private
 
+  def broadcast_append_to_chat
+    broadcast_append_to chat, target: "messages", partial: "messages/message", locals: { message: self }
+  end
+
   def user_message_limit
-    if chat.messages.where(role: "user").count >= MAX_USER_MESSAGES
-      errors.add(:content, "You can only send #{MAX_USER_MESSAGES} messages per chat.")
-    end
+    return unless chat.messages.where(role: "user").count >= MAX_USER_MESSAGES
+
+    errors.add(:content, "You can only send #{MAX_USER_MESSAGES} messages per chat.")
   end
 end
